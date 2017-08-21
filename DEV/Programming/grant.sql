7c8556502c5ef2d87be7f2f69c875d2b900287b2
shawn0915 <shawn2016@aliyun.com>
2017-08-21 16:58:17 +0800

    grant

--- DEV/Programming/grant.sql
+++ DEV/Programming/grant.sql
@@ -1,3 +1,5 @@
 GRANT ALL ON sakila.* TO 'shawn'@'%';
+-- 匹配sakila开头的schema
+GRANT ALL ON `sakila%`.* TO 'shawn'@'%';
 
 GRANT EXECUTE, ALTER ROUTINE ON PROCEDURE sakila.record_count TO 'shawn'@'%';
